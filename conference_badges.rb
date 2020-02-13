3e148c9a26f77f7585e2d9c2908027c851349abd
Frank  D'Angelo <08fdange@gmail.com>
2020-02-13 23:41:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges.push(badge_maker(name))
+  end
+  return badges
+end
+
+def assign_rooms(names)
+  room_assignments = []
+  names.each_with_index do |name, index|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+
+    
+    
+    
